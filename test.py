f1deff787b8e7ac935e5af69c32a8b50e4a2b3b8
kshuakpaev <kshuakpaev@gmail.com>
2023-02-01 19:57:45 +0600

    01.02.2023 6 обновление

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from aiogram import Dispatcher, Bot, types, executor
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 
 bot = Bot(token='5729459971:AAEocY-4n-2PISfSjKOV2SKR5DloZb2goVY')
@@ -9,10 +9,15 @@ async def on_startup(_):
     print('Started...')
 
 
-kb = ReplyKeyboardMarkup(resize_keyboard=True)
-b1 = KeyboardButton(text="/help")
-b2 = KeyboardButton(text="/vote")
+kb = InlineKeyboardMarkup(row_width=2)
+b1 = InlineKeyboardButton(text="Ввод перевозчика", callback_data="b1")
+b2 = InlineKeyboardButton(text="перевозчика", callback_data="b2")
 kb.add(b1, b2)
+
+kb1 = InlineKeyboardMarkup(row_width=2)
+b11 = InlineKeyboardButton(text="Ввод 2 перевозчика", callback_data="b11")
+b21 = InlineKeyboardButton(text="2 перевозчика", callback_data="b21")
+kb1.add(b11, b21)
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
@@ -20,6 +25,9 @@ async def start_command(message: types.Message):
                            text='Welcome!',
                            reply_markup=kb)
 
+@dp.callback_query_handler(lambda call: "b1" in call.data)
+async def next_keyboard(call):
+    await call.message.edit_reply_markup(kb1)
 
 
 if __name__ == "__main__":
